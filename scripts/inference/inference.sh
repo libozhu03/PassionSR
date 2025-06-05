e4f82951f57740c2da0c21af3119213723fc4232
Labor555 <zlb85579327@163.com>
2025-06-05 19:43:04 +0800

    fix bugs

--- scripts/inference/inference.sh
+++ scripts/inference/inference.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES="6" \
+CUDA_VISIBLE_DEVICES="3" \
 python inference_single.py \
-        --config scripts/inference/config/saw_sep/U/saw_W6A6.yaml
+        --config scripts/inference/config/saw_sep/UV/saw_U_W6A6_V_W6A6.yaml
--- scripts/measure/measure.sh
+++ scripts/measure/measure.sh
@@ -1,17 +1,17 @@
-# CUDA_VISIBLE_DEVICES="5" \
-# python measure.py \
-#     -i results/quantize/saw_sep/UV/U_W8A8_V_W8A8/DIV2K_val \
-#     -r ../data/DIV2K/DIV2K_valid_HR
+CUDA_VISIBLE_DEVICES="2" \
+python measure.py \
+    -i results/quantize/saw_sep/UV/U_W8A8_V_W8A8/DIV2K_val \
+    -r ../data/DIV2K/DIV2K_valid_HR
 
-# CUDA_VISIBLE_DEVICES="5" \
-# python measure.py \
-#     -i results/quantize/saw_sep/UV/U_W6A6_V_W6A6/DIV2K_val \
-#     -r ../data/DIV2K/DIV2K_valid_HR
+CUDA_VISIBLE_DEVICES="3" \
+python measure.py \
+    -i results/quantize/saw_sep/UV/U_W6A6_V_W6A6/DIV2K_val \
+    -r ../data/DIV2K/DIV2K_valid_HR
 
-# CUDA_VISIBLE_DEVICES="6" \
-# python measure.py \
-#     -i results/quantize/saw_sep/U/W8A8/DIV2K_val \
-#     -r ../data/DIV2K/DIV2K_valid_HR
+CUDA_VISIBLE_DEVICES="6" \
+python measure.py \
+    -i results/quantize/saw_sep/U/W8A8/DIV2K_val \
+    -r ../data/DIV2K/DIV2K_valid_HR
 
 CUDA_VISIBLE_DEVICES="6" \
 python measure.py \
